66196ce5f27b0fea8af9fb245c9a980040561dd6
lisnarv <valeralisnar30@gmail.com>
2025-03-28 09:54:59 +0100

    correcciones

---
+++ routes/items.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const itemController = require('../controllers/ItemController');
+
+// Endpoints CRUD básicos
+router.get('/', (req, res) => itemController.getItems(req, res));
+router.get('/:id', (req, res) => itemController.getItemById(req, res));
+router.post('/', (req, res) => itemController.createItem(req, res));
+router.put('/:id', (req, res) => itemController.updateItem(req, res));
+router.delete('/:id', (req, res) => itemController.deleteItem(req, res));
+
+// Endpoints adicionales
+router.get('/search', (req, res) => itemController.searchItems(req, res));
+router.put('/bulk-update', (req, res) => itemController.bulkUpdate(req, res));
+router.delete('/delete-all', (req, res) => itemController.deleteAll(req, res));
+
+module.exports = router;
