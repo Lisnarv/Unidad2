178be1004e068cec0b3e4691165e480139348e99
lisnarv <valeralisnar30@gmail.com>
2025-03-28 13:08:26 +0100

    cambios en el archivo items.js

--- routes/items.js
+++ routes/items.js
@@ -2,6 +2,11 @@ const express = require('express');
 const router = express.Router();
 const itemController = require('../controllers/ItemController');
 
+// Endpoints adicionales
+router.get('/search', (req, res) => itemController.searchItems(req, res));
+router.put('/bulk-update', (req, res) => itemController.bulkUpdate(req, res));
+router.delete('/delete-all', (req, res) => itemController.deleteAll(req, res));
+
 // Endpoints CRUD básicos
 router.get('/', (req, res) => itemController.getItems(req, res));
 router.get('/:id', (req, res) => itemController.getItemById(req, res));
@@ -9,9 +14,6 @@ router.post('/', (req, res) => itemController.createItem(req, res));
 router.put('/:id', (req, res) => itemController.updateItem(req, res));
 router.delete('/:id', (req, res) => itemController.deleteItem(req, res));
 
-// Endpoints adicionales
-router.get('/search', (req, res) => itemController.searchItems(req, res));
-router.put('/bulk-update', (req, res) => itemController.bulkUpdate(req, res));
-router.delete('/delete-all', (req, res) => itemController.deleteAll(req, res));
+
 
 module.exports = router;
